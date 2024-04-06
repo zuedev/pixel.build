7c86a37ea8fb1ef2d94f44f940cda8e8c7712c4f
zuedev <zuedev@gmail.com>
2024-04-06 23:36:36 +0100

    add logo for favicon

--- sites/www.pixel.build/src/app/layout.js
+++ sites/www.pixel.build/src/app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
   title: "pixel.build",
   description:
     "Hey there, we're pixel.build – your one-stop shop for bringing awesome props to life. Whether you need a quick 3D print or a crazy, custom-made piece that'll blow minds, we're here to turn your ideas into reality, one pixel at a time.",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
