990496647f1f5681d3c928b60f33d473481dccd6
zuedev <zuedev@gmail.com>
2024-04-06 23:40:36 +0100

    another fix...

--- sites/www.pixel.build/src/app/layout.js
+++ sites/www.pixel.build/src/app/layout.js
@@ -15,9 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <Analytics />
-      <SpeedInsights />
+      <body>
+        <Analytics />
+        <SpeedInsights />
+        {children}
+      </body>
     </html>
   );
 }
