46613274a47a55158dcd983e828af28af0b1b2a0
zuedev <zuedev@gmail.com>
2024-04-06 23:22:12 +0100

    using next js now fancy site ooooh owo!!!!

---
+++ sites/www.pixel.build/src/app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "pixel.build",
+  description:
+    "Hey there, we're pixel.build – your one-stop shop for bringing awesome props to life. Whether you need a quick 3D print or a crazy, custom-made piece that'll blow minds, we're here to turn your ideas into reality, one pixel at a time.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ sites/www.pixel.build/src/app/page.js
@@ -0,0 +1,59 @@
+import "./page.css";
+
+const links = [
+  {
+    name: "About",
+    href: "#",
+  },
+  {
+    name: "Capabilities",
+    href: "#",
+  },
+  {
+    name: "Contact",
+    href: "#",
+  },
+];
+
+const socials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/zuedev/pixel.build",
+    imgSrc: "https://cdn.simpleicons.org/github/fff",
+  },
+  {
+    name: "Discord",
+    href: "https://discord.gg/k7VWbAKTCM",
+    imgSrc: "https://cdn.simpleicons.org/discord",
+  },
+];
+
+export default function Home() {
+  return (
+    <>
+      <video playsInline autoPlay muted loop>
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
+
+      <main>
+        <img id="logo" src="/logo.png" alt="pixel.build Logo" />
+        <div id="links">
+          {links.map((link) => (
+            <a key={link.name} href={link.href}>
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <div id="socials">
+          {socials.map((social) => (
+            <a key={social.name} href={social.href}>
+              <img src={social.imgSrc} alt={social.name} />
+            </a>
+          ))}
+        </div>
+      </main>
+
+      <span id="version">v0.2.1</span>
+    </>
+  );
+}
