2bd755f70f25502943927cc4683eb10c6006049a
zuedev <zuedev@gmail.com>
2024-04-10 00:04:25 +0100

    random background video

--- sites/www.pixel.build/src/components/VideoBackground/index.js
+++ sites/www.pixel.build/src/components/VideoBackground/index.js
@@ -1,5 +1,11 @@
 export default function VideoBackground(props) {
-  let src = props?.src || "/background.mp4";
+  let srcArray = props?.srcArray || [
+    "/4198845-hd_1920_1080_25fps",
+    "/15283169-hd_1080_1920_30fps.mp4",
+    "/11066399-hd_1920_1080_60fps.mp4",
+    "/10222930-hd_1920_1080_30fps.mp4",
+  ];
+  let src = props?.src || srcArray[Math.floor(Math.random() * srcArray.length)];
   let type = props?.type || "video/mp4";
 
   return (
